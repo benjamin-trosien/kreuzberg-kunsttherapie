d3a0a70b1b1290731a26bd64cfaed2288bf54ad5
Benjamin Trosien <benjamin.trosien@gmail.com>
2022-05-07 18:08:08 +0200

    fix project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ml-art-therapy'),
+      dir: require('path').join(__dirname, './coverage/kreuzberg-kunsttherapie'),
       subdir: '.',
       reporters: [
         { type: 'html' },
